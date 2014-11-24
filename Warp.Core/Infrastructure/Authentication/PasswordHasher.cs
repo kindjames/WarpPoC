d38209d41e05049aa047cc97c28a10e46882336f
James Kind <hello@jameskind.com>
2014-11-24 11:12:41 +0000

    Custom PasswordHasher for Identity,

---
+++ Warp.Core/Infrastructure/Authentication/PasswordHasher.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Warp.Core.Infrastructure.Authentication
+{
+    public sealed class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(password + "reGucpbCPtWBgsFKfNtH");
+
+            byte[] hashedBytes;
+
+            using (var hashService = new SHA512CryptoServiceProvider())
+            {
+                hashedBytes = hashService.ComputeHash(inputBytes);
+            }
+
+            return BitConverter.ToString(hashedBytes)
+                .Replace("-", "")
+                .ToLower();
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(string hashedPassword, string providedPassword)
+        {
+            if (String.CompareOrdinal(hashedPassword, HashPassword(providedPassword)) == 0)
+            {
+                return PasswordVerificationResult.Success;
+            }
+
+            return PasswordVerificationResult.Failed;
+        }
+    }
+}
