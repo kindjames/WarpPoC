3c1bb222beb90a1ffff3b36a246ed3dff4c1aa6d
James Kind <hello@jameskind.com>
2014-11-24 16:25:43 +0000

    Added Login and Logout actions.

--- Warp.WebUI/Controllers/AccountController.cs
+++ Warp.WebUI/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ namespace Warp.WebUI.Controllers
     public class AccountController : Controller
     {
         private readonly IAuthenticationManager _authenticationManager;
-        private UserManager<ApplicationUser, int> _userManager;
+        private readonly UserManager<ApplicationUser, int> _userManager;
 
         public AccountController(UserManager<ApplicationUser, int> userManager, IAuthenticationManager authenticationManager)
         {
@@ -42,53 +42,22 @@ namespace Warp.WebUI.Controllers
 
                 if (result.Succeeded)
                 {
-                    await SignInAsync(user, false);
+                    _authenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+
+                    var identity = await _userManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                    _authenticationManager.SignIn(new AuthenticationProperties { IsPersistent = false }, identity);
 
                     return RedirectToAction("Index", "Home");
                 }
 
-                AddErrors(result);
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error);
+                }
             }
 
-            // If we got this far, something failed, redisplay form
             return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Route("log-off")]
-        public ActionResult LogOff()
-        {
-            _authenticationManager.SignOut();
-
-            return RedirectToAction("Index", "Home");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && _userManager != null)
-            {
-                _userManager.Dispose();
-                _userManager = null;
-            }
-            base.Dispose(disposing);
-        }
-
-        private void AddErrors(IdentityResult result)
-        {
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError("", error);
-            }
-        }
-
-        private async Task SignInAsync(ApplicationUser user, bool rememberMe)
-        {
-            _authenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
-
-            var identity = await _userManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
-
-            _authenticationManager.SignIn(new AuthenticationProperties { IsPersistent = rememberMe }, identity);
         }
     }
 }
---
+++ Warp.WebUI/Controllers/AuthenticationController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Warp.Core.Authentication;
+using Warp.WebUI.Models.Authentication;
+
+namespace Warp.WebUI.Controllers
+{
+    [RoutePrefix("authentication")]
+    public class AuthenticationController : Controller
+    {
+        private readonly IAuthenticationManager _authenticationManager;
+        private readonly UserManager<ApplicationUser, int> _userManager;
+
+        public AuthenticationController(UserManager<ApplicationUser, int> userManager, IAuthenticationManager authenticationManager)
+        {
+            _userManager = userManager;
+            _authenticationManager = authenticationManager;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("log-off")]
+        public ActionResult LogOff()
+        {
+            _authenticationManager.SignOut();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AllowAnonymous]
+        [Route("login")]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [Route("login")]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindAsync(model.UserName, model.Password);
+
+                if (user != null)
+                {
+                    _authenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+
+                    var identity = await _userManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                    _authenticationManager.SignIn(new AuthenticationProperties { IsPersistent = model.RememberMe }, identity);
+                    
+                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("", "Invalid username or password.");
+            }
+
+            return View(model);
+        }
+    }
+}
