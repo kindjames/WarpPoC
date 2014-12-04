168f30c3d192bfeb0da9b19d64228d946284973e
Paul G <Paul.Glasson@hospitalitygem.com>
2014-12-04 15:58:47 +0000

    [OTF:1619] Database schema annotations on POCOs

--- Warp.Data.CodeFirst/Entities/RoleGroup.cs
+++ Warp.Data.CodeFirst/Entities/RoleGroup.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Warp.Data.Entities
 {
+    [Table("RoleGroups", Schema = "Client")]
     public class RoleGroup : EntityBase
     {
         public RoleGroup()
--- Warp.Data.CodeFirst/Entities/User.cs
+++ Warp.Data.CodeFirst/Entities/User.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Warp.Data.Entities
 {
+    [Table("Users", Schema = "Client")]
     public class User : EntityBase
     {
         public int UserId { get; set; }
