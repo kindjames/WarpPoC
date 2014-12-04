27d2bd88089e6d92e9cae2da7879f49162f06fc2
Paul G <Paul.Glasson@hospitalitygem.com>
2014-12-04 15:53:52 +0000

    [OTF:1619] Entity Framework Code Migrations

--- Warp.Data.CodeFirst/Migrations/Configuration.cs
+++ Warp.Data.CodeFirst/Migrations/Configuration.cs
@@ -17,18 +17,6 @@ namespace Warp.Data.Migrations
         protected override void Seed(Warp.Data.Migrations.InternalMigrationsInitializationContext context)
         {
             DataFactoryResolver.Seed(context);
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
