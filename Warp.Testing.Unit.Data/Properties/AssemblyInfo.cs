2ab9c6de2df87268ea547d4d8a2285c87df9355f
James Kind <hello@jameskind.com>
2014-12-29 16:24:23 +0000

    Removed rogue TestFactory directory.

--- Warp.Testing.Unit.Data/Properties/AssemblyInfo.cs
+++ Warp.Testing.Unit.Data/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestFactory")]
+[assembly: AssemblyTitle("Warp.Data.Specs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestFactory")]
+[assembly: AssemblyProduct("Warp.Data.Specs")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,8 +18,8 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c885c0f3-2b84-4694-be80-c541c213390b")]
+// The following GUId is for the Id of the typelib if this project is exposed to COM
+[assembly: Guid("8f95804b-eeaa-4813-bf14-47804c55274d")]
 
 // Version information for an assembly consists of the following four values:
 //
