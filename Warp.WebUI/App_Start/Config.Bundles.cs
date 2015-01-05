01d28d5830cf69d14947381422b3670ef2955f8f
James Kind <hello@jameskind.com>
2015-01-05 16:03:55 +0000

    Added flaty css and markup, and font-awesome.

--- Warp.WebUI/App_Start/Config.Bundles.cs
+++ Warp.WebUI/App_Start/Config.Bundles.cs
@@ -6,25 +6,43 @@ namespace Warp.WebUI
     {
         public void ConfigureBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+            // TODO: Add conditional .min versions for prod version.
 
-            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
+            // Bootstrap
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/bootstrap-theme").Include("~/Content/bootstrap-theme.css"));
+            bundles.Add(new StyleBundle("~/css/bootstrap")
+                .Include("~/Content/bootstrap.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/css/bootstrap-theme")
+                .Include("~/Content/bootstrap-theme.css"));
 
-            bundles
-                .Add(new ScriptBundle("~/bundles/jquery-val")
+            // Flaty
+            bundles.Add(new StyleBundle("~/css/flaty")
+                .Include("~/Content/flaty.css")
+                .Include("~/Content/flaty-responsive.css"));
+
+            // jQuery
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js"));
+
+            // jQuery Validation
+            bundles.Add(new ScriptBundle("~/bundles/jquery-val")
                 .Include("~/Scripts/jquery.unobtrusive-ajax.js",
                          "~/Scripts/jquery.validate.js",
                          "~/Scripts/jquery.validate.unobtrusive.js",
                          "~/Scripts/jquery.validate.unobtrusive.bootstrap.js"));
-            bundles
-                .Add(new ScriptBundle("~/bundles/jquery-typewatch")
+            // jQuery TypeWatch
+            bundles.Add(new ScriptBundle("~/bundles/jquery-typewatch")
                 .Include("~/Scripts/jquery.typewatch.js"));
 
-            bundles.Add(new StyleBundle("~/bundles/authentication").Include("~/Content/Styles/Authentication/Login.css"));
+            // Font Awesome
+            bundles.Add(new StyleBundle("~/css/font-awesome")
+                .Include("~/Content/font-awesome.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/authentication")
+                .Include("~/Content/Styles/Authentication/Login.css"));
         }
     }
 }
---
+++ Warp.WebUI/Controllers/DashboardControlsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+
+namespace Warp.WebUI.Controllers
+{
+    public class DashboardControlsController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult TopNavigationBar()
+        {
+            var model = new TopNavigationBarViewModel
+            {
+                ApplicationTitle = "HospitalityGEM"
+            };
+            
+            return PartialView(model);
+        }
+
+        [ChildActionOnly]
+        public ActionResult SideNavigationBar()
+        {
+            var model = new SideNavigationBarViewModel();
+
+            return PartialView(model);
+        }
+    }
+
+    public class SideNavigationBarViewModel
+    {
+    }
+
+    public class TopNavigationBarViewModel
+    {
+        public string ApplicationTitle { get; set; }
+    }
+}
