58d388261e2701a22d07d7c630e3eca76ea4f9f9
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-06 15:06:45 +0000

    [OTF:1619] Updated SaveNewClientCommandHandler test annotation for greater clarity during testing and added Data CommandHandler Unit test files

---
+++ Warp.Testing.Unit.Data/Commands/TextResources/SaveNewTextResourceCommandHandlerUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+using AutoMapper;
+using Machine.Fakes;
+using Machine.Specifications;
+using Warp.Core.Exceptions;
+using Warp.Core.Infrastructure.AutoMapper;
+using Warp.Core.Query;
+using Warp.Data.Commands.Clients;
+using Warp.Data.Context;
+using Warp.Data.Entities;
+using Warp.Data.Queries.Clients;
+using Warp.Data.Queries.General;
+using IObjectMapper = Warp.Core.Infrastructure.AutoMapper.IObjectMapper;
+using MoqIt = Moq.It;
+
+namespace Warp.Testing.Unit.Data.Commands.TextResources
+{
+    [Subject("SaveNewTextResourceCommandHandler Unit Tests")]
+    public static class SaveNewTextResourceCommandHandlerUnitTests
+    {
+    }
+}
