27c2dde011b895a22646b31f8c06423b60c9d780
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-06 17:03:38 +0000

    [OTF:1619] Added Save and Update TextResource commands

---
+++ Warp.Data.CodeFirst/Commands/TextResources/SaveNewTextResourceCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warp.Core.Command;
+using Warp.Core.Infrastructure.AutoMapper;
+using Warp.Core.Query;
+using Warp.Data.Context;
+
+namespace Warp.Data.Commands.TextResources
+{
+    /// <summary>
+    /// Wrapper for SaveTextResourceCommand aggregate
+    /// </summary>
+    public sealed class SaveNewTextResourceCommand : ICommand
+    {
+        public int Id { get; internal set; }
+    }
+
+    public sealed class SaveNewTextResourceCommandHandler : ICommandHandler<SaveNewTextResourceCommand>
+    {
+        private readonly IDomainDbContext _dbContext;
+        private readonly IQueryDispatcher _queryDispatcher;
+        private readonly IObjectMapper _objectMapper;
+
+        public void Execute(SaveNewTextResourceCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
