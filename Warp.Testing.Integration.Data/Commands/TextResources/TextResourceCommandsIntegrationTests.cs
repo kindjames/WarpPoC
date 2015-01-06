9c3f58a2d402975637c60488ee01a76a7a2ecea6
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-06 14:28:34 +0000

    [OTF:1619] Added TextResource Command and Query Integration Test files

---
+++ Warp.Testing.Integration.Data/Commands/TextResources/TextResourceCommandsIntegrationTests.cs
@@ -0,0 +1,12 @@
+﻿using Machine.Fakes;
+using Machine.Specifications;
+using Warp.Core.Services.TextResourceService;
+
+namespace Warp.Testing.Integration.Data.Commands.TextResources
+{
+    [Subject("TextResource Command Integration Tests")]
+    public static class TextResourceCommandsIntegrationTests
+    {
+       
+    }
+}
---
+++ Warp.Testing.Integration.Data/Queries/TextResources/TextResourceQueryIntegrationTests.cs
@@ -0,0 +1,11 @@
+﻿using Machine.Fakes;
+using Machine.Specifications;
+using Warp.Core.Services.TextResourceService;
+
+namespace Warp.Testing.Integration.Data.Queries.TextResources
+{
+    [Subject("Text Resource Query Integration Tests")]
+    public static class TextResourceQueryIntegrationTests
+    {
+    }
+}
