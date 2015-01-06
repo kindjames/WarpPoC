76941e8904dcd015f364a5f6c12da12501fa5798
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-06 17:03:38 +0000

    [OTF:1619] Added Save and Update TextResource commands

---
+++ Warp.Data.CodeFirst/Commands/TextResources/UpdateTextResourceIdentifierCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warp.Core.Command;
+
+namespace Warp.Data.Commands.TextResources
+{
+    //public sealed class UpdateTextResourceIdentifierCommand : ICommand
+    //{
+    //}
+
+    //public sealed class UpdateTextResourceIdentifierCommandHandler : ICommandHandler<UpdateTextResourceIdentifierCommand>
+    //{
+    //}
+}
