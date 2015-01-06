284e99c2ea279a5182f8c9b2ce17786303d1a02b
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-06 17:28:14 +0000

    [OTF:1619] Updated SaveNewTextResource aggregate files

--- Warp.Data.CodeFirst/Commands/TextResources/SaveNewTextResourceCommand.cs
+++ Warp.Data.CodeFirst/Commands/TextResources/SaveNewTextResourceCommand.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Warp.Core.Command;
 using Warp.Core.Infrastructure.AutoMapper;
+using Warp.Core.Infrastructure.Validation;
 using Warp.Core.Query;
 using Warp.Data.Context;
 
@@ -16,6 +18,24 @@ namespace Warp.Data.Commands.TextResources
     public sealed class SaveNewTextResourceCommand : ICommand
     {
         public int Id { get; internal set; }
+
+        // Required TextResourceIdentifier data
+        [Required]
+        public string ResourceIdentifierCode { get; set; }
+
+        [Required]
+        public bool ClientOverridable { get; set; }
+
+        // Required TextResource data
+        [IdRequired]
+        public int LanguageId { get; set; }
+
+        // Populated after successful TextResourceIdentifier validation(Here or in Dispatcher?)
+        public int ResourceIdentifierId { get; set; }
+
+        [Required]
+        public string ResourceString { get; set; }
+
     }
 
     public sealed class SaveNewTextResourceCommandHandler : ICommandHandler<SaveNewTextResourceCommand>
@@ -26,6 +46,15 @@ namespace Warp.Data.Commands.TextResources
 
         public void Execute(SaveNewTextResourceCommand command)
         {
+            // Validate ResourceIdentifierCode
+                // No. Get Associated TextResource data, populate and return DuplicateTextResourceDto to User(ResourceIdentifier and associated TextResource dto)
+                // Yes(No duplicate).
+                    // Validate ResourceString
+                        // No.(Duplicate). Return duplicate data to User(Aggregized ResourceIdentifierDto and associated TextResourceDto)
+                        // Yes.
+                            // Invoke SaveNewResourceIdentifierCommand and return ResourceIdentifierId
+                            // Invoke SaveNewResourceCommand and return TextResourceDetailsDto to User
+
             throw new NotImplementedException();
         }
     }
