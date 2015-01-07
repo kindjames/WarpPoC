ca1a0ec610d64ee80ef6a6e9b971f11a6a398fe9
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-07 14:57:32 +0000

    [OTF:1619] SaveNewTextResource model Command update

---
+++ Warp.Core/Services/Dtos/TextResources/TextResourceValidationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp.Core.Services.Dtos.TextResources
+{
+    public class TextResourceValidationDto
+    {
+        public bool IsResourceCodeValidated { get; set; }
+        public bool IsResourceStringValidated { get; set; }
+    }
+}
