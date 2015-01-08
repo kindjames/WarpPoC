3e0da83a309c351b23c02e53dc72d9f1aced7ad1
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-08 10:33:50 +0000

    [OTF:1619] Added UpdateTextResourceStringCommand

---
+++ Warp.Data.CodeFirst/Commands/TextResources/UpdateTextResourceStringCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warp.Core.Command;
+using Warp.Core.Infrastructure.Validation;
+
+namespace Warp.Data.Commands.TextResources
+{
+    public sealed class UpdateTextResourceStringCommand : ICommand
+    {
+        public int Id { get; set; }
+
+        [IdRequired]
+        public int ResourceIdentifierId { get; set; }
+
+        [Required]
+        public string ResourceString { get; set; }
+    }
+}
