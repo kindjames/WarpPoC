2f235422c7fe44eb301481b14d55ab89430f98da
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-08 09:35:26 +0000

    [OTF:1619] Added and updated SaveTextResource validation, save and update model dtos

---
+++ Warp.Core/Services/Dtos/DtoBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp.Core.Services.Dtos
+{
+    public class DtoBase
+    {
+        public Int32 Id { get; set; }
+    }
+}
