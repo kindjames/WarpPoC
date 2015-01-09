ac2dd9a94c0ae570b77c40c0da828c7e7cb6befc
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-09 09:59:14 +0000

    [OTF:1619] Updated SaveTextResource Validation queries

---
+++ Warp.Data.CodeFirst/Queries/TextResources/ValidateUniqueResourceCodeQuery.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Warp.Core.Query;
+using Warp.Data.Context;
+
+namespace Warp.Data.Queries.TextResources
+{
+    public class ValidateUniqueResourceCodeQuery : IQuery<bool>
+    {
+        [Required]
+        public string ResourceIdentifierCode { get; set; }
+    }
+
+    public class ValidateUniqueResourceCodeQueryHandler : IQueryHandler<ValidateUniqueResourceCodeQuery, bool>
+    {
+        readonly ITextResourceDbContext _dbContext;
+
+        public ValidateUniqueResourceCodeQueryHandler(ITextResourceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool Execute(ValidateUniqueResourceCodeQuery query)
+        {
+            return _dbContext.TextResourceIdentifiers
+                .Any(tri => tri.TextResourceCode == query.ResourceIdentifierCode);
+        }
+    }
+}
