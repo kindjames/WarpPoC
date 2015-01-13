6da9b013efeb3568f71b305a7a24ff405693d22a
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-13 15:15:34 +0000

    [OTF:1619] Added Testing.data project as a container for all test artifacts

---
+++ Warp.Testing.Data/Testing.Data/TestContexts/ContextSpecBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Testing.Data.TestContexts
+{
+    public class ContextSpecBase
+    {
+        [SetUp]
+        public void MainSetup()
+        {
+            SetContext();
+            Because();
+        }
+
+        [TearDown]
+        protected void MainTearDown()
+        {
+            CleanUp();
+        }
+
+        protected abstract void CleanUp();
+
+
+        protected abstract void Because();
+        protected abstract void SetContext();
+    }
+}
