509f4c070c96c23dcfc4dd19b9f3cf41bff41dc5
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-13 15:14:34 +0000

    [OTF:1619] Added Testing.data project as a container for all test artifacts

---
+++ Warp.Testing.Data/Testing.Data/Reference/With_a_new_EditOwnerPresenter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Testing.Data.TestContexts;
+
+namespace Testing.Data.Reference
+{
+    public class With_a_new_EditOwnerPresenter : AAABase
+    {
+        //protected Mock<IEditOwnerView> _mockView;
+
+    }
+}
