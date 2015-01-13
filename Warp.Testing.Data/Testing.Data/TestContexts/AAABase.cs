783949ad6fe75d3a8a0c54411f1a2e59f7dc36ce
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-13 15:15:34 +0000

    [OTF:1619] Added Testing.data project as a container for all test artifacts

---
+++ Warp.Testing.Data/Testing.Data/TestContexts/AAABase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Testing.Data.TestContexts
+{
+    public class AAABase
+    {
+        [SetUp]
+        public void MainSetup()
+        {
+            Arrange();
+            Act();
+        }
+
+        [TearDown]
+        protected void MainTeardown()
+        {
+            CleanUp();
+        }
+
+        protected virtual void Act() { }
+        protected virtual void Arrange() { }
+        protected virtual void CleanUp() { }
+    }
+}
