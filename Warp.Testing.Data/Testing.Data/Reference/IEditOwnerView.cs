413d1b9711b25e0c96ab2e0db2d597b5096ea371
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-13 15:14:34 +0000

    [OTF:1619] Added Testing.data project as a container for all test artifacts

---
+++ Warp.Testing.Data/Testing.Data/Reference/IEditOwnerView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Testing.Data.Reference
+{
+    //protected interface IEditOwnerView
+    //{
+    //}
+
+    //protected interface IDataContext
+    //{
+    //}
+
+    //protected interface ICommandFactory
+    //{
+
+    //}
+}
