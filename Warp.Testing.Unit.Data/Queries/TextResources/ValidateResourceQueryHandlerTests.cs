a1c2a0ebcee423f88055ebd3ab1a1227eb309258
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-16 14:48:08 +0000

    [OTF:1619] TextResource model. Added associated data Dto's

--- Warp.Testing.Unit.Data/Queries/TextResources/ValidateResourceQueryHandlerTests.cs
+++ Warp.Testing.Unit.Data/Queries/TextResources/ValidateResourceQueryHandlerTests.cs
@@ -6,21 +6,23 @@ using System.Text;
 using System.Threading.Tasks;
 using FakeDbSet;
 using Machine.Specifications;
+using Machine.Fakes;
 using Warp.Core.Services.Dtos.TextResources;
 using Warp.Data.Context;
 using Warp.Data.Entities;
 using Warp.Testing.Unit.Data.Commands.TextResources;
 using Warp.Testing.Unit.Data.Queries.Languages;
 
+
 namespace Warp.Testing.Unit.Data.Queries.TextResources
 {
-    [Subject("TextResource Validaation Query Tests")]
+    [Subject("TextResource Validation Query Tests")]
     public class ValidateResourceQueryHandlerTests
     {
         // Test base class
         public class Validate_TextResource_BaseContext
         {
-            protected SaveTextResourceDto _dto;
+            static protected SaveTextResourceDto _dto;
 
             Establish context = () =>
             {
@@ -29,67 +31,67 @@ namespace Warp.Testing.Unit.Data.Queries.TextResources
                     ClientOverridable = false,
                     ClientId = 0,
                     ResourceIdentifierCode = "WelcomeText",
-                    ResourceString = "Welcome!";
+                    ResourceString = "Welcome!"
                 };
             
             // Moq TextResource context
-            The<ITextResourceDbContext>
-                .WhenToldTo(tr => tr.TextResources)
-                .Return(new InMemoryDbSet<TextResource>(true)
-                {
-                    new TextResource
-                    {
-                        Id = 1,
-                        ResourceString = "Welcome!",
-                        TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
-                        Language = new Language {Id = 1},
-                        Client = new Client {Id = 0}
-                    },
-                new TextResource
-                {
-                    Id = 1,
-                    ResourceString = "Log In",
-                    TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
-                    Language = new Language {Id = 1},
-                    Client = new Client {Id = 0}
-                },
-                new TextResource
-                {
-                    Id = 1,
-                    ResourceString = "Password",
-                    TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
-                    Language = new Language {Id = 1},
-                    Client = new Client {Id = 0}
-                },
-                new TextResource
-                {
-                    Id = 1,
-                    ResourceString = "Username",
-                    TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
-                    Language = new Language {Id = 1},
-                    Client = new Client {Id = 0}
-                },
-                new TextResource
-                {
-                    Id = 1,
-                    ResourceString = "Remember me for 2 weeks",
-                    TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
-                    Language = new Language {Id = 1},
-                    Client = new Client {Id = 0}
-                },
-                new TextResource
-                {
-                    Id = 1,
-                    ResourceString = "E-mail or TMDC Id",
-                    TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
-                    Language = new Language {Id = 1},
-                    Client = new Client {Id = 0}
-                }
+                //The<ITextResourceDbContext>()
+                //    .WhenToldTo(tr => tr.TextResources)
+                //    .Return(new InMemoryDbSet<TextResource>(true)
+                //    {
+                //        new TextResource
+                //        {
+                //            Id = 1,
+                //            ResourceString = "Welcome!",
+                //            TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
+                //            Language = new Language {Id = 1},
+                //            Client = new Client {Id = 0}
+                //        },
+                //        new TextResource
+                //        {
+                //            Id = 1,
+                //            ResourceString = "Log In",
+                //            TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
+                //            Language = new Language {Id = 1},
+                //            Client = new Client {Id = 0}
+                //        },
+                //        new TextResource
+                //        {
+                //            Id = 1,
+                //            ResourceString = "Password",
+                //            TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
+                //            Language = new Language {Id = 1},
+                //            Client = new Client {Id = 0}
+                //        },
+                //        new TextResource
+                //        {
+                //            Id = 1,
+                //            ResourceString = "Username",
+                //            TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
+                //            Language = new Language {Id = 1},
+                //            Client = new Client {Id = 0}
+                //        },
+                //        new TextResource
+                //        {
+                //            Id = 1,
+                //            ResourceString = "Remember me for 2 weeks",
+                //            TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
+                //            Language = new Language {Id = 1},
+                //            Client = new Client {Id = 0}
+                //        },
+                //        new TextResource
+                //        {
+                //            Id = 1,
+                //            ResourceString = "E-mail or TMDC Id",
+                //            TextResourceIdentifier = new TextResourceIdentifier {Id = 1},
+                //            Language = new Language {Id = 1},
+                //            Client = new Client {Id = 0}
+                //        }
 
-                }),
-                
-The<TextResourceDbContext>
-.WhenToldTo(tri => tri.TextResourceIdentifiers)
+                //    });
+                    
+                /// TODO -> Add ResourceIdentifier TestMoq
+                /// 
             };
         }
 
