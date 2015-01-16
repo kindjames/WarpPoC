d3d1dd3e15d7b1fe38373e45d578bd45c5c89c94
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-16 14:48:08 +0000

    [OTF:1619] TextResource model. Added associated data Dto's

---
+++ Warp.Data.CodeFirst/Queries/TextResources/GetAssociatedResourceStringDataQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warp.Core.Query;
+using Warp.Core.Services.Dtos.TextResources;
+
+namespace Warp.Data.Queries.TextResources
+{
+    public class GetAssociatedResourceStringDataQuery : IQuery<ValidationResponseDto>
+    {
+        public string ResourceIdentifierCode { get; set; }
+    }
+
+
+}
