1550e3e3414cf5efb4e6524867f6d7e070dde907
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-16 09:12:16 +0000

    [OTF:1619] TextResource model. ValidateResourceQuery tests

---
+++ Warp.Data.CodeFirst/Queries/TextResources/ValidateResourceStringQuery.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Warp.Core.Query;
+using Warp.Data.Context;
+
+namespace Warp.Data.Queries.TextResources
+{
+    public class ValidateResourceStringQuery : IQuery<bool>
+    {
+        [Required]
+        public string ResourceString { get; set; }
+
+        [Required]
+        public int UserLanguageId { get; set; }
+    }
+
+    public class ValidateResourceStringQueryHandler : IQueryHandler<ValidateResourceStringQuery, bool>
+    {
+        private readonly ITextResourceDbContext _dbContext;
+
+        public ValidateResourceStringQueryHandler(ITextResourceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool Execute(ValidateResourceStringQuery query)
+        {
+            return _dbContext.TextResources
+                .Any(tr => tr.ResourceString == query.ResourceString && tr.LanguageId == query.UserLanguageId);
+        }
+    }
+}
