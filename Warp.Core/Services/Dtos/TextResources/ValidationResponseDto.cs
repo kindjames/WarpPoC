202f3d8e5f5a41a4021d8ec963f2f3d3203a82b8
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-16 14:46:42 +0000

    [OTF:1619] TextResource model. Core object Dto update

---
+++ Warp.Core/Services/Dtos/TextResources/ValidationResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp.Core.Services.Dtos.TextResources
+{
+    public sealed class ValidationResponseDto
+    {
+        public IEnumerable<string> Messages { get; set; }
+        public bool IsValidated { get; set; }
+
+        public ValidationResponseDto()
+        {
+            IsValidated = false;
+            Messages = new List<string>();
+        }
+    }
+}
