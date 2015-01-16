8a3e697cb558ad467d360f7de196e3150684f6e7
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-16 14:48:40 +0000

    [OTF:1619] TextResource model. Prototype IValidator interface

---
+++ Warp.Core/Validation/IValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp.Core.Validation
+{
+    public interface IValidator<T>
+    {
+        ValidationResponse Validate(T obj);
+        ValidationResponse Validate(T obj, bool suppressWarnings);
+    }
+
+    public sealed class ValidationMessage
+    {
+        
+    }
+
+    public sealed class ValidationResponse
+    {
+        public IEnumerable<string> Messages { get; set; }
+    }
+
+}
