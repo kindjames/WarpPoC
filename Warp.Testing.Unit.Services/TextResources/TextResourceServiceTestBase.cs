9ce8e63adb0d6363bf11c9230168c5415ab35da6
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-16 09:14:01 +0000

    [OTF:1619] TextResource model. Unit test and TextResourceService test update

---
+++ Warp.Testing.Unit.Services/TextResources/TextResourceServiceTestBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Fakes;
+using Machine.Specifications;
+using Warp.Core.Services.Dtos.Client;
+using Warp.Core.Services.Dtos.TextResources;
+using Warp.Data.Entities;
+using Warp.Services;
+
+namespace Warp.Testing.Unit.Services.TextResources
+{
+    protected abstract class TextResourceServiceTestBase : WithSubject<TextResourceService>
+    {
+        //static TextResource _textResource;
+
+        Because of = () => Subject.SaveTextResource(CreateDto());
+
+        static abstract SaveTextResourceDto CreateDto();
+    }
+
+    [Subject("Saving a new TextResource")]
+    public class When_saving_a_new_TextResource : TextResourceServiceTestBase
+    {
+        override 
+    }
+}
