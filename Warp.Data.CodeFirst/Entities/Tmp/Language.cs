590aa2906abcb430616e0bc60734d09f0b32a610
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-19 14:10:14 +0000

    [OTF:1619] TextResource temporary Unit Tests and artifacts. (Guid pending)

---
+++ Warp.Data.CodeFirst/Entities/Tmp/Language.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp.Data.Entities.Tmp
+{
+    public class Language : EntityBase
+    {
+        [Required]
+        public string InvariantCulture { get; set; }
+
+        [Required]
+        public string Locale { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ Warp.Data.CodeFirst/Entities/Tmp/TextResource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp.Data.Entities.Tmp
+{
+    public class TextResource : EntityBase
+    {
+        public new int Id { get; set; } // 'new' for defining composite-primary-key.
+
+        public int ClientId { get; set; }
+
+        public int LanguageId { get; set; }
+
+        public int ResourceIdentifierId { get; set; }
+
+        [Required]
+        public string ResourceString { get; set; }
+
+        public virtual Client Client { get; set; }
+        public virtual Language Language { get; set; }
+        public virtual TextResourceIdentifier TextResourceIdentifier { get; set; }
+    }
+}
