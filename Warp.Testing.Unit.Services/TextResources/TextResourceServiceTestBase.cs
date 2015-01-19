5f1acbf48c0babe2ae504adf3c57beeac8ac4e17
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-19 09:12:17 +0000

    [OTF:1619] Added prototype 'Establish' context TestBase class

--- Warp.Testing.Unit.Services/TextResources/TextResourceServiceTestBase.cs
+++ Warp.Testing.Unit.Services/TextResources/TextResourceServiceTestBase.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Machine.Fakes;
 using Machine.Specifications;
+using Moq.Language;
 using Warp.Core.Services.Dtos.Client;
 using Warp.Core.Services.Dtos.TextResources;
 using Warp.Data.Entities;
@@ -12,18 +13,18 @@ using Warp.Services;
 
 namespace Warp.Testing.Unit.Services.TextResources
 {
-    protected abstract class TextResourceServiceTestBase : WithSubject<TextResourceService>
+    public class TextResourceServiceTestBase : WithSubject<TextResourceService>
     {
         //static TextResource _textResource;
 
-        Because of = () => Subject.SaveTextResource(CreateDto());
+        //Because of = () => Subject.SaveTextResource(CreateDto());
 
-        static abstract SaveTextResourceDto CreateDto();
+        //SaveTextResourceDto CreateDto();
     }
 
     [Subject("Saving a new TextResource")]
     public class When_saving_a_new_TextResource : TextResourceServiceTestBase
     {
-        override 
+        
     }
 }
