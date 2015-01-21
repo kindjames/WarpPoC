9b9dca4d1a473ed3f8faf21a70bf454d9be30988
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-21 15:29:49 +0000

    [OTF:1619] SaveTextResource Dto implementations

--- Warp.Core/Services/TextResourceService/ITextResourceService.cs
+++ Warp.Core/Services/TextResourceService/ITextResourceService.cs
@@ -7,27 +7,7 @@ namespace Warp.Core.Services.TextResourceService
     {
         string GetTextResource(Guid textResourceCodeId);
         string GetTextResourceFromCode(string textResourceCode);
-        ResourceStringDto GetTextResourceString(Guid textResourceCodeId);
-        ResourceCodeDto GetTextResourceCode(Guid textResourceCodeId);
-
-        /// <summary>
-        /// TODO -> SaveTextResource
-        /// </summary>
-        /// <param name="saveTextResourceDto"></param>
-        void SaveTextResource(SaveTextResourceDto saveTextResourceDto);
         
-        /// <summary>
-        /// TODO -> Validate unique ResourceString
-        /// </summary>
-        /// <param name="textResourceString"></param>
-        /// <returns>bool</returns>
-        bool CheckResourceStringExists(string textResourceString);
-
-        /// <summary>
-        ///  TODO -> Validate unique ResourceCode
-        /// </summary>
-        /// <param name="textResourceCode"></param>
-        /// <returns>bool</returns>
-        bool CheckResourceCodeExists(string textResourceCode);
+        IResponse<TextResourceDetailDto> SaveTextResource(SaveTextResourceDto saveTextResourceDto);
     }
 }
