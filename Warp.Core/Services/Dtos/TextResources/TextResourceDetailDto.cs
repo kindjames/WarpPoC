61b14539260257c5d6acadb2abf0edfd90fe597d
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-21 15:29:49 +0000

    [OTF:1619] SaveTextResource Dto implementations

---
+++ Warp.Core/Services/Dtos/TextResources/TextResourceDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Warp.Core.Services.Dtos.TextResources
+{
+    public class TextResourceDetailDto
+    {
+        public string ResourceString { get; set; }
+        public string ResourceIdentifierCode { get; set; }
+
+        public string LanguageName { get; set; }
+        public string LanguageCulture { get; set; }
+
+        public string ClientName { get; set; }
+    }
+}
