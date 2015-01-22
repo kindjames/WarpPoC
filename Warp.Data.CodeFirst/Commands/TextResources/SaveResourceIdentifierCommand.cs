6acfa2c4faedfb7409c70c3c6e632a5b8aa38bc6
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-22 16:58:18 +0000

    [OTF:1619] SaveTextResource Command model refactor and implementation

--- Warp.Data.CodeFirst/Commands/TextResources/SaveResourceIdentifierCommand.cs
+++ Warp.Data.CodeFirst/Commands/TextResources/SaveResourceIdentifierCommand.cs
@@ -4,22 +4,29 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 using Warp.Core.Cqrs;
 using Warp.Core.Infrastructure.AutoMapper;
-using Warp.Core.Util;
+using Warp.Core.Infrastructure.Util;
+using Warp.Core.Infrastructure.Validation;
 using Warp.Data.Context;
 
 namespace Warp.Data.Commands.TextResources
 {
     public sealed class SaveResourceIdentifierCommand : ICommand
     {
-        [Required]
-        public string ResourceIdentifierCode { get; set; }
-
-        [Required]
-        public bool ClientOverridable { get; set; }
-
         public Guid Id { get; set; }
+        public string ResourceIdentifierCode { get; set; }
+        public bool ClientOverridable { get; set; }
+    }
+
+    public sealed class SaveResourceIdentifierCommandValidator : AbstractValidator<SaveResourceIdentifierCommand>
+    {
+        public SaveResourceIdentifierCommandValidator()
+        {
+            RuleFor(c => c.ResourceIdentifierCode).NotEmpty().WithMessage("A Resource Identifier Code is required. e.g. 'WelcomeText'");
+            RuleFor(c => c.ClientOverridable).NotEmpty().NotNull().WithMessage("Please specify if this TextResource is Client overridable.");
+        }
     }
 
     public sealed class SaveResourceIdentifierCodeCommandHandler : ICommandHandler<SaveResourceIdentifierCommand>
