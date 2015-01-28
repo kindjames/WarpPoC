0781c667e666cdbe91532b0114c8f45320f66c47
Paul G <Paul.Glasson@hospitalitygem.com>
2015-01-28 08:36:17 +0000

    [1619] Prototype BDD implementation

---
+++ Warp.Testing.Integration.Services/Warp.Testing.Integration.Services/DeveloperSavesATextResourceSteps.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Warp.Testing.Integration.Services
+{
+    [Binding]
+    public class DeveloperSavesATextResourceSteps
+    {
+        [Given(@"I have TextResource data to persist to the database")]
+        public void GivenIHaveTextResourceDataToPersistToTheDatabase()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"the ResourceString is unique")]
+        public void GivenTheResourceStringIsUnique()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"the ResourceIdentifierCode is unique")]
+        public void GivenTheResourceIdentifierCodeIsUnique()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        //        [Given(@"the Resource string is unique")]
+        //public void GivenTheResourceStringIsUnique()
+        //{
+        //    ScenarioContext.Current.Pending();
+        //}
+
+        [Given(@"the ResourceIdentifierCode is duplicated")]
+        public void GivenTheResourceIdentifierCodeIsDuplicated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"the Resource string is duplicated")]
+        public void GivenTheResourceStringIsDuplicated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the ResourceString data has been checked for uniqueness")]
+        public void WhenTheResourceStringDataHasBeenCheckedForUniqueness()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the CheckIsResourceStringUniqueQuery returns true")]
+        public void WhenTheCheckIsResourceStringUniqueQueryReturnsTrue()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the ResourceString has been successfully checked for uniqueness")]
+        public void WhenTheResourceStringHasBeenSuccessfullyCheckedForUniqueness()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the query returns false")]
+        public void WhenTheQueryReturnsFalse()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the ResourceIdentifier data is checked for uniqueness")]
+        public void ThenTheResourceIdentifierDataIsCheckedForUniqueness()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the CheckIsResourceIdentifierCodeUniqueQuery returns true")]
+        public void ThenTheCheckIsResourceIdentifierCodeUniqueQueryReturnsTrue()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the TextResourceService initialises a SaveTextResourceCommand from the SaveTextResourceDto")]
+        public void ThenTheTextResourceServiceInitialisesASaveTextResourceCommandFromTheSaveTextResourceDto()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the TextResourceIdentifier data is persisted to the TextResourceIdentifier table")]
+        public void ThenTheTextResourceIdentifierDataIsPersistedToTheTextResourceIdentifierTable()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the TextResource data is persisted to the TextResource table")]
+        public void ThenTheTextResourceDataIsPersistedToTheTextResourceTable()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"SaveChanges\(\) is called on the model")]
+        public void ThenSaveChangesIsCalledOnTheModel()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the CheckIsResourceIdentifierCodeUniqueQuery returns false")]
+        public void ThenTheCheckIsResourceIdentifierCodeUniqueQueryReturnsFalse()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the TextResourceService creates a SaveResourceResponseDto")]
+        public void ThenTheTextResourceServiceCreatesASaveResourceResponseDto()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"runs a GetResourceIdentifierCodedataQuery against the duplicated ResourceIdentifierCode string")]
+        public void ThenRunsAGetResourceIdentifierCodedataQueryAgainstTheDuplicatedResourceIdentifierCodeString()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"returns the duplicate ResourceIdentifierCode data")]
+        public void ThenReturnsTheDuplicateResourceIdentifierCodeData()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"runs the GetAssociatedTextResourceDataQuery using the ResourceIdentifierCodeId")]
+        public void ThenRunsTheGetAssociatedTextResourceDataQueryUsingTheResourceIdentifierCodeId()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"populates the SaveResourceResponseDto with the result of the associated data dto")]
+        public void ThenPopulatesTheSaveResourceResponseDtoWithTheResultOfTheAssociatedDataDto()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"maps the SaveResourceResponseDto to a SaveResourceResponseViewModel")]
+        public void ThenMapsTheSaveResourceResponseDtoToASaveResourceResponseViewModel()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"returns the mapped Viewmodel")]
+        public void ThenReturnsTheMappedViewmodel()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"runs a query for associated ResourceIdentifier data")]
+        public void ThenRunsAQueryForAssociatedResourceIdentifierData()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"populates the SaveResourceResponseDto with the result of the associated data query")]
+        public void ThenPopulatesTheSaveResourceResponseDtoWithTheResultOfTheAssociatedDataQuery()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ Warp.Testing.Integration.Services/Warp.Testing.Integration.Services/TextResources/SaveTextResource.feature
@@ -0,0 +1,70 @@
+﻿Feature: Developer Saves a Text Resource
+	As a Developer
+	I want to be able to verify the uniqeness of new TextResource data
+	So that I can persist the data to the database
+
+@SaveTextResource
+@WiP
+@1_UniqueResourceString_UniqueIdentifierCode
+Scenario: Successful Save with unique data
+	Given I have TextResource data to persist to the database
+		And the ResourceString is unique
+		And the ResourceIdentifierCode is unique
+	When the ResourceString data has been checked for uniqueness
+		And the CheckIsResourceStringUniqueQuery returns true
+		Then the ResourceIdentifier data is checked for uniqueness
+		And the CheckIsResourceIdentifierCodeUniqueQuery returns true
+	Then the TextResourceService initialises a SaveTextResourceCommand from the SaveTextResourceDto
+		And the TextResourceIdentifier data is persisted to the TextResourceIdentifier table
+		And the TextResource data is persisted to the TextResource table
+	Then SaveChanges() is called on the model
+
+
+@2_UniqueResourceString_DuplicateResourceIdentifierCode
+Scenario: Failed Save with unique ResourceString and a duplicate ResourceIdentifierCode
+	Given I have TextResource data to persist to the database
+		And the Resource string is unique
+		And the ResourceIdentifierCode is duplicated
+		# Validation cycle
+	When the ResourceString data has been checked for uniqueness
+		And the CheckIsResourceStringUniqueQuery returns true
+	Then the ResourceIdentifier data is checked for uniqueness
+		And the CheckIsResourceIdentifierCodeUniqueQuery returns false
+		#Query duplicator allocated data query cycle
+	Then the TextResourceService creates a SaveResourceResponseDto
+		And runs a GetResourceIdentifierCodedataQuery against the duplicated ResourceIdentifierCode string
+		And returns the duplicate ResourceIdentifierCode data
+	Then runs the GetAssociatedTextResourceDataQuery using the duplicate ResourceIdentifierCodeId
+		And returns associated TextResource data
+		
+	
+	Then populates the SaveResourceResponseDto with the result of the associated data dto
+		And maps the SaveResourceResponseDto to a SaveResourceResponseViewModel
+	Then returns the mapped Viewmodel
+
+"""
+
+#@3_UniqueResourceString_DifferentResourceIdentifierCode
+#Scenario: Failed Save
+
+@_DuplicateResourceString_UniqueResourceIdentifier
+Scenario: Unsuccessful Save with duplicate ResourceString and a unique ResourceIdentifierCode
+	Given I have TextResource data to persist to the database
+		And the Resource string is duplicated
+		And the ResourceIdentifierCode is unique
+	When the ResourceString has been successfully checked for uniqueness
+		And the query returns false
+	Then the TextResourceService creates a SaveResourceResponseDto
+		And runs a query for associated ResourceIdentifier data
+	Then populates the SaveResourceResponseDto with the result of the associated data query
+		And maps the SaveResourceResponseDto to a SaveResourceResponseViewModel
+	Then returns the mapped Viewmodel
+
+#Scenario: Unsuccessful Save with unique TextResource string and a duplicate ResourceIdentifierCode
+
+
+#@WorstCaseScenario
+#Scenario: Unsuccessful Save with a duplicate TextResource string and a duplicate ResourceIdentifierCode
+
+#@ignore
+"""
